54c701b051a7ab6b08103a1cf82149e722f843c3
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-01-29 12:18:14 -0500

    first commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//include library 
+#include <iostream>
+//read through library using std 
+using namespace std;
+//create main function using int
+int main(void)
+//output main function
+{
+    cout << "Chasing Cars" << endl;
+    return 0;
+}
