2eabb45a0379ebb98c72c69038e7dc577ce06080
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-01-29 16:13:56 -0500

    Created two functions

--- display.cpp
+++ display.cpp
@@ -1,14 +1,18 @@
+// import library
 #include <iostream>
+// using std library
 using namespace std;
 
+// tip for compiler
 void display(void);
-
+// create main function
 int main(void)
 {
+    // call function
     display();
     return 0;
 }
-
+// function declaration 
 void display(void)
 {
     cout << "You reached the display function" << endl;
