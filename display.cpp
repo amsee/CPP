172f36b9aa9a73b049b8f0978508b710ffa171b8
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-01-29 15:48:42 -0500

    Called a function

---
+++ display.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void display(void);
+
+int main(void)
+{
+    display();
+    return 0;
+}
+
+void display(void)
+{
+    cout << "You reached the display function" << endl;
+}
