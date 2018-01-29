f7902f4d3777b1fe845bd4790336991eedc1202a
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-01-29 13:58:36 -0500

    Vital information about turtles.

--- turtles.cpp
+++ turtles.cpp
@@ -16,13 +16,13 @@ int main(void)
     cout << "How many turtles do you have?: " << endl;
     cin >> turtles; 
 
-    cout << " How many nostrils do they have?: " << endl;
+    cout << "How many nostrils do they have?: " << endl;
     cin >> nostrils;
 
-    cout << " How many lives do they have left?: " << endl; 
+    cout << "How many lives do they have left?: " << endl; 
     cin >> life;
 
-    cout << " How many times a day does it take a washroom break?: " << endl;
+    cout << "How many times a day does it take a washroom break?: " << endl;
     cin >> feces;
     
     return 0;
