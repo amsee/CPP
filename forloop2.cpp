e9bc83d1f7ae52bef05e2ea22fa4687f03df76b8
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-02-02 17:12:28 -0500

    For loop practice

---
+++ forloop2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // local variable initialization
+    int person = 50;
+    // output to user
+    cout << "Here are your numbers: ";
+    cin >> person;
+    // execute for loop -> init, condition; increment
+    for (int zxc = person; zxc > person; zxc++)
+    {
+        cout << person << zxc + 1 << endl;
+
+    }
+
+    return 0;
+
+}
